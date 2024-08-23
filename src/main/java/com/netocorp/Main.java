36159e72e78df7ff7876a7a73a88f6519374b50d
dfneto <pdavidfneto@gmail.com>
2024-08-23 17:46:04 +0200

    developing the person class

--- src/main/java/com/netocorp/Main.java
+++ src/main/java/com/netocorp/Main.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -14,8 +17,51 @@ public class Main {
 
     @GetMapping("/greet")
     public GreetResponse greet() {
-        return new GreetResponse("Hello");
+        return new GreetResponse(
+                "Hello",
+                List.of("Go", "C", "Java"),
+                new Person("David", 38, 1_000_000));
     }
 
-    public record GreetResponse(String greet) {}
+    record Person(String name, int age, double savings) {}
+
+    public record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person) {}
+
+
+//    This code is equivalent to:
+//    public record GreetResponse(String greet) {}
+//    public class GreetResponse {
+//        private final String greet;
+//
+//        public GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hashCode(greet);
+//        }
+//    }
 }
